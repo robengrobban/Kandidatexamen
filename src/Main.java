04a09afac69753c1f7f56161d8e8f5ef443355d7
Robert Englund <kastanjen3@gmail.com>
2022-04-10 20:41:01 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
-    private static final int[] NUMBER_OF_THREADS = { 1, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48 };
-    private static final int[] NUMBER_OF_ELEMENTS = { 32768, 1048576 };
+    private static final int[] NUMBER_OF_THREADS = { 1, 3, 6, 12, 24, 48 };
+    private static final int[] NUMBER_OF_ELEMENTS = { 32768, 185363, 1048576 };
     private static final OperationsDistribution[] Operations = {
             OperationsDistribution.HigRead(),
             OperationsDistribution.HighMod(),
